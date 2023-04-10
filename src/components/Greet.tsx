af1991868ba503b00722e77cda76cc6057cf261f
Quan Pham <quapham27@gmail.com>
2023-04-10 13:50:52 +0700

    Basic Props

---
+++ src/components/Greet.tsx
@@ -0,0 +1,18 @@
+type GreetProps = {
+    name: string
+    numCount: number
+    isLoggedIn: boolean
+}
+
+export const Greet = (props: GreetProps) => {
+    return (
+        <div>
+            <h2>
+                {props.isLoggedIn
+                    // eslint-disable-next-line no-template-curly-in-string
+                    ? 'Welcome ${props.name}! You have ${props.numCount} unread messages'
+                    : 'Welcome Guest'}
+            </h2>
+        </div>
+    )
+}
