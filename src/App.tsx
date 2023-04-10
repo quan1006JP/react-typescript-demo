a9c7f9192496de071151a7c1b0e3d3ed4a43f7f1
Quan Pham <quapham27@gmail.com>
2023-04-10 14:16:52 +0700

    Advanced Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import "./App.css";
-import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ];
-
   return (
     <div className="App">
-      <Greet name="User" numCount={10} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList}/>
+      <Status status='success'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Quan goes to Alta</Heading>
+      </Oscar>
     </div>
   );
 }
